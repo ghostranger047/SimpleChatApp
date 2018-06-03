15692abf68e3889d1c1dc5aacda7dfd0848090d3
ghostranger047 <niladrisarkar047@hotmail.com>
2018-06-03 16:27:18 +0530

    first push

---
+++ Chat.py
@@ -0,0 +1,44 @@
+﻿# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'Chat.ui'
+#
+# Created by: PyQt5 UI code generator 5.10.1
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(633, 467)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.centralwidget)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.msgArea = QtWidgets.QTextEdit(self.centralwidget)
+        self.msgArea.setObjectName("msgArea")
+        self.verticalLayout.addWidget(self.msgArea)
+        self.horizontalLayout = QtWidgets.QHBoxLayout()
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.msg = QtWidgets.QPlainTextEdit(self.centralwidget)
+        self.msg.setMaximumSize(QtCore.QSize(16777215, 25))
+        self.msg.setObjectName("msg")
+        self.horizontalLayout.addWidget(self.msg)
+        self.send = QtWidgets.QPushButton(self.centralwidget)
+        self.send.setObjectName("send")
+        self.horizontalLayout.addWidget(self.send)
+        self.verticalLayout.addLayout(self.horizontalLayout)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.send.setText(_translate("MainWindow", "Send"))
+
---
+++ ChatControl.py
@@ -0,0 +1,65 @@
+﻿from PyQt5 import QtWidgets, QtCore, QtGui
+from PyQt5.QtWidgets import QWidget, QDesktopWidget, QApplication
+from Chat import Ui_MainWindow
+import sys
+import os
+from socket import AF_INET, socket, SOCK_STREAM
+from threading import Thread
+
+class ChatControl(QtWidgets.QMainWindow, Ui_MainWindow):
+    edit = None
+    saveloc = ""
+    def __init__(self):
+        super(ChatControl, self).__init__()
+        self.setupUi(self)
+        self.send.clicked.connect(self.sendMsg)
+        self.HOST = input('Enter host: ')
+        self.PORT = input('Enter port: ')
+        if not self.PORT:
+            self.PORT = 33000
+        else:
+            self.PORT = int(self.PORT)
+
+        self.BUFSIZ = 1024
+        self.ADDR = (self.HOST, self.PORT)
+
+        self.client_socket = socket(AF_INET, SOCK_STREAM)
+        self.client_socket.connect(self.ADDR)
+
+        self.receive_thread = Thread(target=self.receive)
+        self.receive_thread.start()
+
+
+    def sendMsg(self):
+        msg = self.msg.toPlainText()
+        self.msg.clear()  # Clears input field.
+        self.client_socket.send(bytes(msg, "utf8"))
+        if msg == "{quit}":
+            self.client_socket.close()
+            self.top.quit()
+
+
+    def receive(self):
+        """Handles receiving of messages."""
+        while True:
+            try:
+                msg = self.client_socket.recv(self.BUFSIZ).decode("utf8")
+                self.msgArea.append(msg)
+            except OSError:  # Possibly client has left the chat.
+                break
+
+
+
+     
+
+    #----Now comes the sockets part----
+    
+
+
+
+if __name__ == "__main__":
+    
+    app = QtWidgets.QApplication(sys.argv)
+    form = ChatControl()
+    form.show()
+    sys.exit(app.exec_())
